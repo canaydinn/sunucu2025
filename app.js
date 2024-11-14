287c33ece5e1f836c51e53ef25fcd41e3afe2a27
canaydinn <canaydinn@gmail.com>
2024-11-14 15:09:33 +0300

    mysql bağlantısı yapıldı

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require("express")
+require('dotenv/config')
+const db_connect=require("./db/mysql_connect")
+const app=express()
+const port=process.env.PORT
+/* / işareti index.html*/
+app.get("/",(req,res)=>{
+    res.json({
+        message:"Hoş geldiniz"
+    })
+})
+app.listen(port,()=>{
+    console.log(`Sunucu ${port} portunda çalışıyor`)
+})
