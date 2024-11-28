f0e5903654fe18e0660dee0c69ed7797ac24b5fb
canaydinn <canaydinn@gmail.com>
2024-11-28 15:15:50 +0300

    register

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv/config')
 const db_connect=require("./db/mysql_connect")
 const router=require("./router")
 const app=express()
-app.use("/api",router)
 const port=process.env.PORT
 /* / işareti index.html*/
 app.get("/",(req,res)=>{
@@ -11,6 +10,12 @@ app.get("/",(req,res)=>{
         message:"Hoş geldiniz"
     })
 })
+app.use(express.json({
+    limit:"50mb",extended:true,parameterLimit:50000
+}))
+
+app.use("/api",router)
+
 app.listen(port,()=>{
     console.log(`Sunucu ${port} portunda çalışıyor`)
 })
--- controller/controller.js
+++ controller/controller.js
@@ -1,7 +1,39 @@
+const dbconn=require("../db/mysql_connect")
+
 const kullanici_kontrol=async(req,res)=>{
     console.log(req.body)
+    return res.json(req.body)
 }
 const kullanici_ekle=async(req,res)=>{
-    console.log(req.body)
+    const kullanici_adi=req.body.kullanici_adi
+    const sifre=req.body.sifre
+    const eposta=req.body.eposta
+    const adi=req.body.adi
+    const soyadi=req.body.soyadi
+    const tel_no=req.body.tel_no
+    const cinsiyet=req.body.cinsiyet
+    const dogum_tarihi=req.body.dogum_tarihi
+    const yas=req.body.yas
+
+    dbconn.query("SELECT * FROM kullanicilar WHERE eposta=? AND sifre=?",[eposta,sifre],
+        (error,results)=>{
+            if(results.length>0){
+                return res.status(201).json({
+                    success:true,
+                    data:res.body,
+                    message:"Kayıt Mevcut"
+                });
+                next()
+            }else{
+                dbconn.query(`INSERT INTO kullanicilar (kullanici_adi,sifre,eposta,adi,soyadi,tel_no,cinsiyet,dogum_tarihi,yas) VALUES ("${kullanici_adi}","${sifre}","${eposta}","${adi}","${soyadi}","${tel_no}","${cinsiyet}","${dogum_tarihi}","${yas}")`,(error,results)=>{
+                    return res.status(201).json({
+                        success:true,
+                        data:res.body,
+                        message:"Kayıt Başarılı Bir şekilde Yapıldı"
+                    });                    
+                })
+            }
+        }
+    )
 }
 module.exports={kullanici_kontrol,kullanici_ekle}
