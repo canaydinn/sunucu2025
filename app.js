7765e20b5bbf6d5b7fcb718cdc8eec11bc217dbb
canaydinn <canaydinn@gmail.com>
2024-12-12 13:43:27 +0300

    bodyparser

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express=require("express")
+const bodyParser=require("body-parser")
+const path=require("path")
 require('dotenv/config')
 const router=require("./router")
 const errorHandler=require(".//middleware/errorHandler")
@@ -6,6 +8,8 @@ require("express-async-errors")
 const app=express()
 const port=process.env.PORT
 app.use(errorHandler)
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.json())
 app.use(express.json({limit:"50mb",extended:true,parameterLimit:50000}))
 app.use("/api",router)
 
